5b6515649f752eaf1fa9ac082ee2907f8a052cd2
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2021-04-25 21:38:50 +0200

    super duper update

--- flaskpackage/calculation.py
+++ flaskpackage/calculation.py
@@ -1,5 +1,5 @@
 from math import tan
 
 def calculate(height, angle):
-    return height*tan(angle)
+    return height*tan(angle*0.01745329252)
 
--- flaskpackage/routes.py
+++ flaskpackage/routes.py
@@ -2,15 +2,16 @@ from flask import render_template, session
 from flaskpackage import app, db
 from flaskpackage.models import Calculation
 from flaskpackage.forms import CalculateTrangleForm
-from calculation import calculate
+from .calculation import calculate
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = CalculateTrangleForm()
+    session['RESULT'] = None
     if form.validate_on_submit():
-        calculation = Calculation(height=form.height.data, angle=form.angle.data)
-        db.session.add(calculation)
-        db.session.commit()
-        session['RESULT'] = calculate(form.height.data, form.angle.data)
+        #calculation = Calculation(height=form.height.data, angle=form.angle.data)
+        #db.session.add(calculation)
+        #db.session.commit()
+        session['RESULT'] = calculate(float(form.height.data), float(form.angle.data))
     return render_template('home.html', form=form, result=session['RESULT'])
