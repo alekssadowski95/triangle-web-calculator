f367568d463f925d98423700b60299d22457a147
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2021-04-25 23:11:45 +0200

    Update routes.py

--- flaskpackage/routes.py
+++ flaskpackage/routes.py
@@ -8,10 +8,10 @@ from .calculation import calculate
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = CalculateTrangleForm()
-    session['RESULT'] = None
     if form.validate_on_submit():
         #calculation = Calculation(height=form.height.data, angle=form.angle.data)
         #db.session.add(calculation)
         #db.session.commit()
-        session['RESULT'] = round(calculate(float(form.height.data), float(form.angle.data)), 3)
-    return render_template('home.html', form=form, result=session['RESULT'])
+        result = round(calculate(float(form.height.data), float(form.angle.data)), 3)
+        return render_template('home.html', form=form, result=result)
+    return render_template('home.html', form=form)
